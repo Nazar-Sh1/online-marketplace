967398f264e1dfd2587b1f62dbfbd93471a27f18
Nazar-Sh1 <shchur.nazar1@gmail.com>
2024-09-13 23:08:44 +0300

    adding js

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function init() {
+   // import(""); вписати назву файлу js
+    import("./main.js");
+  }
+  
+  const totalPartials = document.querySelectorAll(
+    '[hx-trigger="load"], [data-hx-trigger="load"]'
+  ).length;
+  let loadedPartialsCount = 0;
+  
+  document.body.addEventListener("htmx:afterOnLoad", () => {
+    loadedPartialsCount++;
+    if (loadedPartialsCount === totalPartials) {
+      init();
+    }
+  });
+  
