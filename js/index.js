dfd2787919140ecf33d4dff46f40bc560d82313e
Shu7naz <128987776+Nazar-Sh1@users.noreply.github.com>
2024-10-27 00:21:59 +0300

    adding a header for the page index.html

--- js/index.js
+++ js/index.js
@@ -1,17 +1,16 @@
 function init() {
-   // import(""); вписати назву файлу js
-    import("./main.js");
+  // import(""); вписати назву файлу js
+  import("./main.js");
+}
+
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener("htmx:afterOnLoad", () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) {
+    init();
   }
-  
-  const totalPartials = document.querySelectorAll(
-    '[hx-trigger="load"], [data-hx-trigger="load"]'
-  ).length;
-  let loadedPartialsCount = 0;
-  
-  document.body.addEventListener("htmx:afterOnLoad", () => {
-    loadedPartialsCount++;
-    if (loadedPartialsCount === totalPartials) {
-      init();
-    }
-  });
-  
+});
