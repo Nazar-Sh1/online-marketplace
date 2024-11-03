b79063cbf18560f9f0dc7adc7de7fe74268b9376
Shu7naz <128987776+Nazar-Sh1@users.noreply.github.com>
2024-11-03 02:39:24 +0200

    adding carousel

--- js/main.js
+++ js/main.js
@@ -43,3 +43,21 @@ document.addEventListener("DOMContentLoaded", () => {
     const items = document.querySelector('.carousel__items');
     items.style.transform = `translateX(-${currentIndex * 33.333}%)`;
 });
+
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
